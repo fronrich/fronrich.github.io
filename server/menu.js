067247b31e39ffdc547241639e79454891872fbe
Fronrich Puno <fronrich@utexas.edu>
2019-08-03 03:24:23 -0500

    Refactoring for js compatibility

---
+++ server/menu.js
@@ -0,0 +1,4 @@
+// JavaScript which will rearrange buttons based on location in page
+console.log("menu.js is active");
+// list of menu items
+var menuItems = document.getElementById("menu-items").getElementsByClassName("button");
